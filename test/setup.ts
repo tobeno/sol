ca3045bb64be6cfb93e271d509ec7229c9a2c693
Tobias Laber <tobeno@gmx.de>
2021-10-27 01:50:47 +0200

    Fix tests

--- test/setup.ts
+++
@@ -1,7 +0,0 @@
-import { globals } from './globals';
-import {
-  definePropertiesMutation,
-  mutateGlobals,
-} from '../dist/src/modules/utils/mutation';
-
-mutateGlobals(definePropertiesMutation(globals));
