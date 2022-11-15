981cbda0f8cc49faf90f13b53973660f2c3371e6
Tobias Laber <tobeno@gmx.de>
2022-11-15 17:33:05 +0100

    Add CLI command

---
+++ src/modules/cli/commands.ts
@@ -0,0 +1,9 @@
+export function runCommand(command: string, args: string[]): void {
+  switch (command) {
+    case 'play':
+      playFile(args[0] || null).replay();
+      break;
+    default:
+      throw new Error(`Unknown command ${command}`);
+  }
+}
