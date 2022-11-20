72aecce346ff0551643a51bb93442b089117bfad
Tobias Laber <tobeno@gmx.de>
2022-11-20 12:07:44 +0100

    Add HTML and XML handling

--- src/modules/data/html.ts
+++ src/modules/data/html.ts
@@ -1,22 +1,74 @@
 import { Wrapper } from './wrapper';
 import { Text } from './text';
 import { DataFormat } from './data-format';
-import type { CheerioAPI } from 'cheerio';
+import type { AnyNode, Element, Text as TextNode } from 'domhandler';
+import type { selectAll, selectOne } from 'css-select';
 import { inspect } from 'util';
+import { Data } from './data';
 
-export class Html extends Wrapper<CheerioAPI> {
-  constructor(value: string, public format: string | null = null) {
-    const cheerio = require('cheerio') as typeof import('cheerio');
-
-    super(cheerio.load(value));
-  }
-
+export class Html<
+  NodeType extends AnyNode = AnyNode,
+> extends Wrapper<NodeType> {
   get text(): Text {
-    return Text.create(this.value.html(), DataFormat.Html);
+    const { default: render } =
+      require('dom-serializer') as typeof import('dom-serializer');
+
+    return Text.create(render(this.value), DataFormat.Html);
   }
 
-  select(selector: Parameters<CheerioAPI>[0]) {
-    return this.value(selector);
+  get type() {
+    return this.value.type;
+  }
+
+  get attributes(): Data<Record<string, string>> {
+    return Data.create((this.value as Element).attribs || {});
+  }
+
+  get content(): Text {
+    if (this.value.type === 'text') {
+      const node = this.value as TextNode;
+
+      return Text.create(node.data);
+    }
+
+    return Text.create(
+      this.filter((n) => n.type === 'text')
+        .value.map((n) => n.content.value.trim())
+        .join('\n'),
+    );
+  }
+
+  attribute(name: string): Text | null {
+    const value = (this.value as Element).attribs?.[name];
+
+    return value ? Text.create(value) : null;
+  }
+
+  filter(cb: (n: AnyNode) => boolean): Data<Html[]> {
+    const DomUtils = require('domutils') as typeof import('domutils');
+
+    return Data.create(
+      DomUtils.filter(cb, this.value).map((e) => Html.create(e)),
+    );
+  }
+
+  find(cb: (n: AnyNode) => boolean): Html | null {
+    return this.filter(cb).value[0] || null;
+  }
+
+  select(selector: Parameters<typeof selectOne>[0]): Html | null {
+    const CSSselect = require('css-select') as typeof import('css-select');
+    const element = CSSselect.selectOne(selector, this.value);
+
+    return element ? Html.create(element) : null;
+  }
+
+  selectAll(selector: Parameters<typeof selectAll>[0]): Data<Html[]> {
+    const CSSselect = require('css-select') as typeof import('css-select');
+
+    return Data.create(
+      CSSselect.selectAll(selector, this.value).map((e) => Html.create(e)),
+    );
   }
 
   /**
@@ -30,11 +82,21 @@ export class Html extends Wrapper<CheerioAPI> {
     return this.text.value;
   }
 
-  static create(value: Text | String | string | any): Html {
+  static create(value: Text | String | AnyNode | string | any): Html {
     if (value instanceof Html) {
       return value;
     }
 
-    return new Html(String(value));
+    if (value instanceof Text || value instanceof String) {
+      value = value.toString();
+    }
+
+    if (value && typeof value === 'object') {
+      return new Html(value);
+    }
+
+    const htmlparser2 = require('htmlparser2') as typeof import('htmlparser2');
+
+    return new Html(htmlparser2.parseDocument(value));
   }
 }
--- src/modules/data/xml.ts
+++ src/modules/data/xml.ts
@@ -1,26 +1,72 @@
-import { Wrapper } from './wrapper';
 import { Text } from './text';
-import { DataFormat } from './data-format';
-import type { CheerioAPI } from 'cheerio';
 import { inspect } from 'util';
+import { DataFormat } from './data-format';
+import { Data } from './data';
+import type { AnyNode, Element, Text as TextNode } from 'domhandler';
+import type { selectAll, selectOne } from 'css-select';
+import { Wrapper } from './wrapper';
 
-export class Xml extends Wrapper<CheerioAPI> {
-  constructor(value: string, public format: string | null = null) {
-    const cheerio = require('cheerio') as typeof import('cheerio');
+export class Xml<NodeType extends AnyNode = AnyNode> extends Wrapper<NodeType> {
+  get text(): Text {
+    const { default: render } =
+      require('dom-serializer') as typeof import('dom-serializer');
 
-    super(
-      cheerio.load(value, {
-        xml: true,
-      }),
+    return Text.create(render(this.value), DataFormat.Html);
+  }
+
+  get type() {
+    return this.value.type;
+  }
+
+  get attributes(): Data<Record<string, string>> {
+    return Data.create((this.value as Element).attribs || {});
+  }
+
+  get content(): Text {
+    if (this.value.type === 'text') {
+      const node = this.value as TextNode;
+
+      return Text.create(node.data);
+    }
+
+    return Text.create(
+      this.filter((n) => n.type === 'text')
+        .value.map((n) => n.content.value.trim())
+        .join('\n'),
     );
   }
 
-  get text(): Text {
-    return Text.create(this.value.html(), DataFormat.Html);
+  attribute(name: string): Text | null {
+    const value = (this.value as Element).attribs?.[name];
+
+    return value ? Text.create(value) : null;
   }
 
-  select(selector: Parameters<CheerioAPI>[0]) {
-    return this.value(selector);
+  filter(cb: (n: AnyNode) => boolean): Data<Xml[]> {
+    const DomUtils = require('domutils') as typeof import('domutils');
+
+    return Data.create(
+      DomUtils.filter(cb, this.value).map((e) => Xml.create(e)),
+    );
+  }
+
+  find(cb: (n: AnyNode) => boolean): Xml | null {
+    return this.filter(cb).value[0] || null;
+  }
+
+  select(selector: Parameters<typeof selectOne>[0]): Xml | null {
+    const CSSselect = require('css-select') as typeof import('css-select');
+    const element = CSSselect.selectOne(selector, this.value);
+
+    return element ? Xml.create(element) : null;
+  }
+
+  selectAll(selector: Parameters<typeof selectAll>[0]): Data<Xml[]> {
+    const CSSselect = require('css-select') as typeof import('css-select');
+
+    return Data.create(
+      CSSselect.selectAll(selector, this.value).map((e) => Xml.create(e)),
+    );
   }
 
   /**
@@ -34,11 +80,25 @@ export class Xml extends Wrapper<CheerioAPI> {
     return this.text.value;
   }
 
-  static create(value: Text | String | string | any): Xml {
+  static create(value: Text | String | AnyNode | string | any): Xml {
     if (value instanceof Xml) {
       return value;
     }
 
-    return new Xml(String(value));
+    if (value instanceof Text || value instanceof String) {
+      value = value.toString();
+    }
+
+    if (value && typeof value === 'object') {
+      return new Xml(value);
+    }
+
+    const htmlparser2 = require('htmlparser2') as typeof import('htmlparser2');
+
+    return new Xml(
+      htmlparser2.parseDocument(value, {
+        xmlMode: true,
+      }),
+    );
   }
 }
