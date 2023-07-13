0e7d30574f0a3697381986a02fb31b6a81197bb3
Tobias Laber <tobeno@gmx.de>
2023-07-13 21:04:54 +0200

    Make stream utils async

--- src/utils/stream.utils.ts
+++ src/utils/stream.utils.ts
@@ -1,16 +1,13 @@
 import { Stream } from 'stream';
-import { awaitSync } from './async.utils';
 
-export function readTextStream(stream: Stream): string {
-  return awaitSync(
-    new Promise((resolve) => {
-      const chunks: string[] = [];
-      stream.on('data', (chunk) => {
-        chunks.push(chunk.toString());
-      });
-      stream.on('end', () => {
-        resolve(chunks.join(''));
-      });
-    }),
-  );
+export async function readTextStream(stream: Stream): Promise<string> {
+  return new Promise((resolve) => {
+    const chunks: string[] = [];
+    stream.on('data', (chunk) => {
+      chunks.push(chunk.toString());
+    });
+    stream.on('end', () => {
+      resolve(chunks.join(''));
+    });
+  });
 }
