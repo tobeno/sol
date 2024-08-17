ac10a1de1309a5069c2eea9b46640fff3442fe05
Tobias Laber <tobeno@gmx.de>
2024-08-17 16:55:43 +0200

    Fix file cache handling

--- src/utils/cache.utils.ts
+++ src/utils/cache.utils.ts
@@ -1,17 +1,18 @@
 import { getCurrentSolWorkspace } from '../sol/sol-workspace';
 import { File } from '../wrappers/file.wrapper';
+import { Wrapper } from '../wrappers/wrapper.wrapper';
 
 const cache = new Map<string | symbol, any>();
 
 /**
  * Cache the result of the given function using the given key in a runtime (variable) cache.
  */
-export function runtimeCached<ResultType = any>(
+export async function runtimeCached<ResultType = any>(
   key: string | symbol,
-  fn: () => ResultType,
-): ResultType {
+  fn: () => ResultType | Promise<ResultType>,
+): Promise<ResultType> {
   if (!cache.has(key)) {
-    const value = fn();
+    const value = await fn();
     cache.set(key, value);
     return value;
   }
@@ -22,23 +23,27 @@ export function runtimeCached<ResultType = any>(
 /**
  * Cache the result of the given function using the given key in a runtime (variable) cache.
  */
-export function fileCached<ResultType = any>(
+export async function fileCached<ResultType = any>(
   pathOrFile: string | File,
-  fn: () => ResultType,
-): ResultType {
+  fn: () => ResultType | Promise<ResultType>,
+): Promise<ResultType> {
   if (typeof pathOrFile === 'string' && !pathOrFile.match(/[/\\]/)) {
     pathOrFile = getCurrentSolWorkspace().cacheDir.file(pathOrFile);
   }
 
   const file = File.create(pathOrFile);
-  if (!file.length) {
-    const result = fn();
+  file.dir.create();
+
+  if (!file.exists) {
+    const result = await fn();
+    if (result instanceof Wrapper) {
+      throw new Error('Cannot cache a wrapper instance.');
+    }
+
     file.json = {
       data: result,
       createdAt: new Date().toISOString(),
     };
-
-    return result;
   }
 
   return file.json.value.data;
